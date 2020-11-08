875ddb8fd0818061b42790c61e540bf3290ca12f
rahulkunder <43509925+rahulkunder@users.noreply.github.com>
2020-11-09 02:38:20 +0530

    Add files via upload

--- aws.py
+++ aws.py
@@ -14,30 +14,65 @@ def aws_menu():
 
 aws_logo = """\033[33m
 
-     ___        ______  
-    / \ \      / / ___| 
+	 ___        ______  
+	/ \ \      / / ___| 
    / _ \ \ /\ / /\___ \ 
   / ___ \ V  V /  ___) |
  /_/   \_\_/\_/  |____/ 
-                        
-	                           
+						
+							   
 	\033[97m """
 
 def aws_list():
 	print("""\033[33m
 				\033[32mChoose Your Option
 
-		\033[36m[1] EC2 Instances  
-		\033[36m[2] EBS
-		\033[36m[3] Connect EC2 to EBS  
-		\033[36m[4] Key Pair
-		\033[36m[5] Security Group
-		\033[91m[6] New feature will added......
+		\033[36m[1] Configure AWS  
+		\033[36m[2] Create Key Pair
+		\033[36m[3] Create Security Group  
+		\033[36m[4] Launch an Instance
+		\033[36m[5] Describe an Instance
+		\033[91m[6] Return to Menu
 	\033[97m """)
 
-	y = input("Choice ==>>  ")
+	ch = int(input("Choice ==>>  "))
+	if int(ch) == 1:
+		print()
+		os.system("aws configure")
+		input("Press Enter to continue")
+	elif int(ch) == 2:
+		print()
+		key_name = input("Enter Key-pair Name :")
+		os.system("aws ec2 create-key-pair --key-name {}".format(key_name))
+		input("Press Enter to continue")
 
-
+	elif int(ch) == 3:
+		print()
+		sg_name = input("Enter Security Group Name : ")
+		sg_desc = input("Enter the Description :")
+		os.system("aws ec2 create-security-group --group-name {0} --description {1}".format(sg_name, sg_desc))
+		print("Add Rule to Security Group")
+		protocol = input("Enter the Protocol : ")
+		port = input("Enter the Port Number : ")
+		cidr = input("Enter the CIDR : ")
+		os.system('aws ec2 authorize-security-group-ingress --group-name {0} --protocol {1} --port {2} --cidr {3}'.format(sg_name, protocol, port, cidr))
+		input("Press Enter to continue")
+	elif int(ch) == 4:
+		print()
+		instance_type = input("Enter teh Instance Type : ")
+		key_name = input("Enter the Key-pair Name : ")
+		count = input("Enter the Count : ")
+		os.system("aws ec2 run-instances --image-id ami-0e306788ff2473ccb --instance-type {0} --key-name {1} --subnet-id subnet-0a420fc4eb459022a --count {2}".format(instance_type, key_name, count))
+		input("Press Enter to continue")
+	elif int(ch) == 5:
+		print()
+		os.system("aws ec2 describe-instances")
+		input("Press Enter to continue")
+	elif int(ch) == 6:
+		exit()
+	else:
+		print("Please Select right Choice")
+		input()
 
 
 aws_menu()
